57bc9888ec553deb2d2d5558296b7fbd26b4fcb0
kdmaniosy <kdmaniosy@ufpso.edu.co>
2024-11-22 12:50:03 -0500

    integracion con vue

---
+++ src/components/vue/MyComponent.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = "Pepito"
+let counter = ref(5)
+
+let characters = ref([])
+
+function add(){
+    counter.value++
+    characters.value.push(counter.value)
+}
+function rest(){
+    counter.value--
+    console.log(counter)
+}
+
+console.log("Hola mundo", name)
+
+</script>
+
+
+<template>
+    <div class="mx-auto max-w-96 rounded bg-blue-300 py-4 px-6">
+        Hola {{ name }}
+        <div v-if="counter > 0" class="size-8 text-white bg-red-600 rounded-full flex items-center justify-center">
+            {{ counter }}
+        </div>
+        <div v-else>
+            Nadie te escribe 😢
+        </div>
+        <div @click="add()" class="size-8 text-white bg-blue-600 rounded-full flex items-center justify-center">
+            +
+        </div>
+        <div @click="rest()" class="size-8 text-white bg-green-600 rounded-full flex items-center justify-center">
+            -
+        </div>
+    </div>
+    <div v-for="(name, index) in characters" :key="index" class="bg-green-500 mb-1">
+        {{ name }}
+    </div>
+
+</template>
